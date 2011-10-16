3947ad8797f733072ec263f2a949695b08ed9db8
unknown <Keith Anderson@PC556411137441.(none)>
2011-10-16 09:27:42 -0700

    fill out category descriptions

--- db/migrate/20111016161012_update_category_descriptions.rb
+++ db/migrate/20111016161012_update_category_descriptions.rb
@@ -1,67 +1,67 @@
 class UpdateCategoryDescriptions < ActiveRecord::Migration
   def up
     c = Category.find_by_name("Sports")
-    c.description = ""
+    c.description = "Peewee, Varsity, Clubs, Professional, and Blood."
     c.save!
 
     c = Category.find_by_name("Government")
-    c.description = ""
+    c.description = "For the people, by the people."
     c.save!
 
     c = Category.find_by_name("Music")
-    c.description = ""
+    c.description = "Concerts, Clubs, DJs, and the Sound of."
     c.save!
 
     c = Category.find_by_name("Arts")
-    c.description = ""
+    c.description = "Multi-Disciplinary/Media, Performing, Graphic, and Garfunkel."
     c.save!
 
     c = Category.find_by_name("Education")
-    c.description = ""
+    c.description = "Teach the people, help them grow."
     c.save!
 
     c = Category.find_by_name("Theater")
-    c.description = ""
+    c.description = "All the world's a stage."
     c.save!
 
     c = Category.find_by_name("Nightlife")
-    c.description = ""
+    c.description = "Clubs, Parties, Bars, and Werewolves."
     c.save!
 
     c = Category.find_by_name("Training")
-    c.description = ""
+    c.description = "Improving yourslef and learning skills."
     c.save!
 
     c = Category.find_by_name("Entertainment")
-    c.description = ""
+    c.description = "Not boring."
     c.save!
 
     c = Category.find_by_name("Civic")
-    c.description = ""
+    c.description = "Enhancing public life."
     c.save!
 
     c = Category.find_by_name("Volunteer")
-    c.description = ""
+    c.description = "Opportunities to give back."
     c.save!
 
     c = Category.find_by_name("Literature")
-    c.description = ""
+    c.description = "My baby loves a bunch of writers."
     c.save!
 
     c = Category.find_by_name("Tech")
-    c.description = ""
+    c.description = "Science, Technology and Math."
     c.save!
 
     c = Category.find_by_name("Comedy")
-    c.description = ""
+    c.description = "It makes you laugh, it makes you cry."
     c.save!
 
     c = Category.find_by_name("Concert")
-    c.description = ""
+    c.description = "Free Bird!"
     c.save!
 
     c = Category.find_by_name("Meeting")
-    c.description = ""
+    c.description = "Circling the wagons."
     c.save!
 
   end
